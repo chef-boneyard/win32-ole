98f7bf740c9c219e69bf1c1d2387c1ab0719d417
Daniel Berger <djberg96@gmail.com>
2014-02-15 03:55:53 -0700

    Added more functions, fixed typedefs.

--- lib/win32/ole/functions.rb
+++ lib/win32/ole/functions.rb
@@ -4,7 +4,8 @@ module Windows
   module Functions
     extend FFI::Library
 
-    typedef :hresult, :long
+    typedef :long, :hresult
+    typedef :ulong, :dword
 
     ffi_lib :ole32
 
@@ -12,6 +13,13 @@ module Windows
     attach_function :CLSIDFromString, [:buffer_in, :pointer], :hresult
     attach_function :CoCreateInstance, [:pointer, :pointer, :dword, :pointer, :pointer], :hresult
     attach_function :CoInitialize, [:pointer], :hresult
+    attach_function :CreateBindCtx, [:dword, :pointer], :hresult
+    attach_function :MkParseDisplayName, [:pointer, :buffer_in, :pointer, :pointer], :hresult
+    attach_function :OleInitialize, [:pointer], :hresult
+
+    ffi_lib :oleaut32
+
+    attach_function :GetActiveObject, [:pointer, :pointer, :pointer], :hresult
 
     # Macros
 
