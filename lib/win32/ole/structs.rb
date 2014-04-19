5263e32da5bad3d679ac2cfb8b6f3bebb1dc2f40
Daniel Berger <djberg96@gmail.com>
2014-04-18 20:02:55 -0600

    Minor comment update.

--- lib/win32/ole/structs.rb
+++ lib/win32/ole/structs.rb
@@ -11,7 +11,7 @@ module Windows
         :Data1, :ulong,       # First 8 hex digits
         :Data2, :ushort,      # First group of 4 hex digits
         :Data3, :ushort,      # Second group of 4 hex digits
-        :Data4, [:byte, 8]    # First 2 bytes contain the 3rd group of 4 hex digits. Remaining 6 bytes contain the # final 12 hex digits.
+        :Data4, [:byte, 8]    # First 2 bytes contain the 3rd group of 4 hex digits. Remaining 6 bytes contain the final 12 hex digits.
       )
     end
   end
