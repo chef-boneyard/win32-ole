6d6d89cb3fc69e77506b6e7317a06538a48dea43
Daniel Berger <djberg96@gmail.com>
2014-05-13 06:57:40 -0600

    Overloaded constructor to allow arguments.

--- lib/win32/ole/structs.rb
+++ lib/win32/ole/structs.rb
@@ -11,8 +11,16 @@ module Windows
         :Data1, :ulong,       # First 8 hex digits
         :Data2, :ushort,      # First group of 4 hex digits
         :Data3, :ushort,      # Second group of 4 hex digits
-        :Data4, [:byte, 8]    # First 2 bytes contain the 3rd group of 4 hex digits. Remaining 6 bytes contain the final 12 hex digits.
+        :Data4, [:uchar, 8]   # First 2 bytes contain the 3rd group of 4 hex digits. Remaining 6 bytes contain the final 12 hex digits.
       )
+
+      def initialize(long = nil, short1 = nil, short2 = nil, *bytes)
+        super()
+        self[:Data1] = long if long
+        self[:Data2] = short1 if short1
+        self[:Data3] = short2 if short2
+        self[:Data4] = bytes.pack("C*") if !bytes.empty?
+      end
 
       def vtable
       end
