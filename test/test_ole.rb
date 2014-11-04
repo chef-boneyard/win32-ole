897327c352c79c56db1f0c8189aa9be58aa1c7fe
Daniel Berger <djberg96@gmail.com>
2014-11-04 09:59:36 -0700

    Formatting tweaks.

--- test/test_ole.rb
+++ test/test_ole.rb
@@ -9,39 +9,39 @@ require 'test/unit'
 include Win32
 
 class TC_Win32_OLE < Test::Unit::TestCase
-   def setup
-      @server = 'InternetExplorer.Application'
-      @host   = Socket.gethostname
-   end
-   
-   def test_constructor
-      assert_respond_to(OLE, :new)
-      assert_nothing_raised{ OLE.new(@server) }
-      assert_nothing_raised{ OLE.new(@server, @host) }
-   end
-   
-   def test_constructor_expected_errors
-      assert_raise(ArgumentError){ OLE.new }
-      assert_raise(ArgumentError){ OLE.new(@server, @host, true) }
-   end
-   
-   def test_constructor_expected_security_errors
-      proc do
-         $SAFE = 1
-         @server.taint
-         assert_raise(SecurityError){ OLE.new(@server) }
-      end.call
-      
-      proc do
-         $SAFE = 1
-         @host.taint
-         server = 'InternetExplorer.Application'
-         assert_raise(SecurityError){ OLE.new(server, @host) }
-      end.call      
-   end
-   
-   def teardown
-      @server = nil
-      @host   = nil
-   end
+  def setup
+    @server = 'InternetExplorer.Application'
+    @host   = Socket.gethostname
+  end
+
+  def test_constructor
+    assert_respond_to(OLE, :new)
+    assert_nothing_raised{ OLE.new(@server) }
+    assert_nothing_raised{ OLE.new(@server, @host) }
+  end
+
+  def test_constructor_expected_errors
+    assert_raise(ArgumentError){ OLE.new }
+    assert_raise(ArgumentError){ OLE.new(@server, @host, true) }
+  end
+
+  def test_constructor_expected_security_errors
+    proc do
+      $SAFE = 1
+      @server.taint
+      assert_raise(SecurityError){ OLE.new(@server) }
+    end.call
+
+    proc do
+      $SAFE = 1
+      @host.taint
+      server = 'InternetExplorer.Application'
+      assert_raise(SecurityError){ OLE.new(server, @host) }
+    end.call
+  end
+
+  def teardown
+    @server = nil
+    @host   = nil
+  end
 end
