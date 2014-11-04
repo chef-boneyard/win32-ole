7fd1a4e9aaedfb10658f1152434862221223f317
Daniel Berger <djberg96@gmail.com>
2014-11-04 13:54:26 -0700

    Added stub Variant class.

---
+++ lib/win32/ole/variant.rb
@@ -0,0 +1,27 @@
+module Win32
+  class OLE
+    # Just a stub for now
+    class Variant
+      def initialize(obj, variant_type = nil)
+        # var = VARIANT.new
+        # VariantInit(var)
+        # check_variant_type(obj)
+      end
+
+      def value
+      end
+
+      def value=(val)
+      end
+
+      def vartype
+      end
+
+      def []
+      end
+
+      def []=(val)
+      end
+    end
+  end
+end
