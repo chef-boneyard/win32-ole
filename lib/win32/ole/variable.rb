1e154ba2e63378484c1276c2e12782921b67fd94
Daniel Berger <djberg96@gmail.com>
2014-11-05 05:34:09 -0700

    Added variable stub class.

---
+++ lib/win32/ole/variable.rb
@@ -0,0 +1,30 @@
+module Win32
+  class OLE
+    # Just a stub class for now.
+    class Variable
+      def initialize
+      end
+
+      def name
+      end
+
+      def ole_type
+      end
+
+      def ole_type_detail
+      end
+
+      def value
+      end
+
+      def visible?
+      end
+
+      def variable_kind
+      end
+
+      def varkind
+      end
+    end
+  end
+end
