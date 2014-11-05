5e809c6bb669e6bc12d3a9e8aa7d6b0827823c6d
Daniel Berger <djberg96@gmail.com>
2014-11-05 05:31:38 -0700

    Added Method class stub.

---
+++ lib/win32/ole/method.rb
@@ -0,0 +1,60 @@
+module Win32
+  class OLE
+    # Just a stub for now.
+    class Method
+      def initialize(ole_type, ole_method)
+      end
+
+      def name
+      end
+
+      def return_type
+      end
+
+      def return_vtype
+      end
+
+      def return_type_detail
+      end
+
+      def invoke_kind
+      end
+
+      def invkind
+      end
+
+      def visible?
+      end
+
+      def event?
+      end
+
+      def event_interface
+      end
+
+      def helpstring
+      end
+
+      def helpfile
+      end
+
+      def helpcontext
+      end
+
+      def dispid
+      end
+
+      def offset_vtbl
+      end
+
+      def size_params
+      end
+
+      def size_opt_params
+      end
+
+      def params
+      end
+    end
+  end
+end
