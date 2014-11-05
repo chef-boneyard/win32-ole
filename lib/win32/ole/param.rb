9704b3399e8de20a3e95e8d7f805bfbae9ce26bf
Daniel Berger <djberg96@gmail.com>
2014-11-05 05:28:33 -0700

    Added param stub.

---
+++ lib/win32/ole/param.rb
@@ -0,0 +1,39 @@
+module Win32
+  class OLE
+    # Just a stub class for now.
+    class Param
+      def initialize(obj1, obj2)
+      end
+
+      def name
+      end
+
+      def ole_type
+      end
+
+      def ole_type_detail
+      end
+
+      def input?
+      end
+
+      def output?
+      end
+
+      def optional?
+      end
+
+      def retval?
+      end
+
+      def default
+      end
+
+      #def to_s
+      #end
+
+      #def inspect
+      #end
+    end
+  end
+end
