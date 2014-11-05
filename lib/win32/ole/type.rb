83a4b3ce13f18309b41352818188676177a4920b
Daniel Berger <djberg96@gmail.com>
2014-11-05 05:41:01 -0700

    Added Type class stub.

---
+++ lib/win32/ole/type.rb
@@ -0,0 +1,24 @@
+module Win32
+  class OLE
+    # Just a stub for now.
+    class Type
+      def initialize
+      end
+
+      def name
+      end
+
+      def ole_type
+      end
+
+      def guid
+      end
+
+      def progid
+      end
+
+      def visible?
+      end
+    end
+  end
+end
